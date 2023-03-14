c9ed4efda9a524eccfa41e55e9e33835db15fe97
Sandro Schnetzer <sandro.schnetzer@skidata.com>
2023-03-14 15:33:44 +0100

    implemented new bit macro

--- utils/bit.h
+++ utils/bit.h
@@ -1,17 +1,21 @@
 #pragma once 
 
 
-/** @return a word with only bit-th bit set. */
+/** @return a byte with only bit-th bit set. */
 #define BIT(bit) (1u << bit)
 
-/** Raise bit-th bit in word. */
-#define BIT_SET(word, bit) ((word) |= BIT(bit))
+/** Raise bit-th bit in byte. */
+#define BIT_SET(byte, bit) ((byte) |= BIT(bit))
 
-/** Clear bit-th bit in word. */
-#define BIT_CLR(word, bit) ((word) &= ~BIT(bit))
+/** Clear bit-th bit in byte. */
+#define BIT_CLR(byte, bit) ((byte) &= ~BIT(bit))
 
-/** Toggle the bit-th bit in word. */
-#define BIT_TOGGLE(word, bit) ((word) ^= BIT(bit))
+/** Toggle the bit-th bit in byte. */
+#define BIT_TOGGLE(byte, bit) ((byte) ^= BIT(bit))
 
-/** @return value "1" if bit-th bit of word is set and zero otherwise. */
-#define BIT_IS_SET(y, pos) ((0u == ((y) & (BIT(pos)))) ? 0u : 1u)
+/** Override bit-th bit in byte with val (true / false). */
+#define BIT_OVERRIDE_WITH_VAL(byte, bit, val) ((val) ? BIT_SET(byte, bit) : BIT_CLR(byte, bit))
+
+/** @return 1 if bit-th bit of byte is set and 0 otherwise. */
+#define BIT_IS_SET(byte, bit) ((0u == ((y) & (BIT(pos)))) ? 0u : 1u)
+
